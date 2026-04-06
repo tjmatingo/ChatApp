63d525308b733b18b44f8c5d3cc7f8c49a828536
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-04-06 12:33:45 +0200

    real time chat section model data manipulation in html

--- rt_chat/views.py
+++ rt_chat/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import *
 
 
+@login_required
 def chat_view(request):
-    return render(request, 'rt_chat/chat.html')
+    chat_group = get_object_or_404(ChatGroup, group_name="School-GC")
+    chat_msgs = ChatGroup.chat_msgs.all()[:30]
+    return render(request, 'rt_chat/chat.html', {"chat_msgs": chat_msgs})
