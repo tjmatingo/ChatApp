618578aab775faa13d8dd668a7180f7cae68824a
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-04-10 23:14:30 +0200

    model setup for private chat rooms and button addition to html and url connection with change to page to chatrrom

--- rt_chat/urls.py
+++ rt_chat/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path('', chat_view, name='home'),
     path('chat/<username>', get_or_create_chatroom, name="start-chat"),
+    path('chat/room/<chatroom_name>', chat_view, name="chatroom"),
 ]
