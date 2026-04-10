9833faab720ea897a5a682a602ef809fef2f6660
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-04-10 23:05:54 +0200

    private chat functionality with members in each chat group

--- rt_chat/models.py
+++ rt_chat/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+import shortuuid
 
 class ChatGroup(models.Model):
     group_name = models.CharField(max_length=128, unique=True)
-    users_online = models.ManyToManyField(User, related_name='online_in_groups', blank=True)
+    users_online = models.ManyToManyField(User, related_name='online_in_groups', blank=True, default=shortuuid.uuid)
+    members = models.ManyToManyField(User, related_name='chat_groups', blank=True)
+    is_private = models.BooleanField(default=False)
 
     def __str__(self):
         return self.group_name
