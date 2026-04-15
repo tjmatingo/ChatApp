15d722a5c6ebec4be873677be56539958aa62a5c
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-04-15 18:28:29 +0200

    paperclip styling and download link for documents uplaoded

--- rt_chat/models.py
+++ rt_chat/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 import shortuuid
+import os
 
 class ChatGroup(models.Model):
     group_name = models.CharField(max_length=128, unique=True, default=shortuuid.uuid)
@@ -21,8 +22,27 @@ class GroupMessage(models.Model):
     file = models.FileField(upload_to="files/", blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f'{self.author.username}: {self.body}'
+    @property
+    def filename(self):
+        if self.file:
+            return os.path.basename(self.file.name)
+        else:
+            return None
+    
 
+    def __str__(self):
+        if self.body: 
+            return f'{self.author.username}: {self.body}'
+        elif self.file:
+            return f'{self.author.username}: {self.filename}'
+            
     class Meta:
         ordering = ['-created']
+
+    @property
+    def is_image(self):
+        if self.filename.lower().endswith(('.jpg', '.jpeg', '.png', '.gif', '.svg', '.webp')):
+            return True
+        else:
+            return False
+    
